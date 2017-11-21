145413f73da3e88d1f285739c5e43dadda659496
Administrator <Administrator@DJA-PC>
2017-11-21 18:31:40 +0900

    project init

---
+++ exam/src/p01/Exam01.java
@@ -0,0 +1,14 @@
+package p01;
+
+public class Exam01 {
+	static int a = 1;
+	static int b = 1;
+	
+	public static void main(String[] args)	{
+		double a = 3;
+		double b = 4;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a/b);
+		}
+}
